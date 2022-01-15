0f57403ce47c2bc5ac49f146429414a883a86bfe
Moacyr Santana <junior.vilasboas@gmail.com>
2022-01-15 21:04:41 +0000

    iniciando o servidor

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const pages = require('./routes/pages')
+
+mongoose.Promise = global.Promise
+
+const app = express()
+const port = process.env.PORT || 3000
+const mongo = process.env.MONGODB || 'mongodb://localhost/minhas-series'
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use('/', pages)
+
+mongoose
+  .connect(mongo, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log('Listening on port: ' + port)
+    })
+  })
+  .catch( e => {
+    console.log(e)
+  })
