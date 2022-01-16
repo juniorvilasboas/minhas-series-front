fa0d42ce44795c169cf491baec8f3804acccbb6d
Moacyr Santana <junior.vilasboas@gmail.com>
2022-01-16 16:45:33 +0000

    add excluir e editar series

--- controllers/series.js
+++ controllers/series.js
@@ -1,6 +1,12 @@
+const labels = [
+  { id: 'to-watch', name: 'Para Assistir', desc: 'bg-success' },
+  { id: 'watching', name: 'Assistindo', desc: 'bg-warning text-dark' },
+  { id: 'watched', name: 'Assistido', desc: 'bg-danger' }
+]
+
 const index = ({ Serie }, req, res) => {
   Serie.find({}, (err, docs) => {
-    res.render('series/index', { series: docs })
+    res.render('series/index', { series: docs, labels })
   })
 }
 
@@ -15,8 +21,34 @@ const novaForm = (req, res) => {
   res.render('series/nova')
 }
 
+const editarProcess = ({ Serie }, req, res) => {
+  Serie.findOne({ _id: req.params.id }, (err, serie) => {
+    serie.name = req.body.name
+    serie.status = req.body.status
+    serie.save()
+    res.redirect('/series')
+  })
+}
+
+const editarForm = ({ Serie }, req, res) => {
+  Serie.findOne({ _id: req.params.id }, (err, serie) => {
+    res.render('series/editar', { serie, labels })
+  })
+}
+
+const excluir = ({ Serie }, req, res) => {
+  Serie.deleteOne({
+    _id: req.params.id
+  }, (err) => {
+    res.redirect('/series')
+  })
+}
+
 module.exports = {
   index,
   novaProcess,
-  novaForm
+  novaForm,
+  editarProcess,
+  editarForm,
+  excluir
 }
--- routes/series.js
+++ routes/series.js
@@ -11,5 +11,8 @@ const models = {
 router.get('/', seriesController.index.bind(null, models))
 router.get('/nova', seriesController.novaForm)
 router.post('/nova', seriesController.novaProcess.bind(null, models))
+router.get('/editar/:id', seriesController.editarForm.bind(null, models))
+router.post('/editar/:id', seriesController.editarProcess.bind(null, models))
+router.get('/excluir/:id', seriesController.excluir.bind(null, models))
 
 module.exports = router
