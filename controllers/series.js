c52fde0dc5a36763d139b14a1b213ce2f10aadc9
Moacyr Santana <junior.vilasboas@gmail.com>
2022-01-16 17:35:31 +0000

    adicionando validação em atualizando layout

--- controllers/series.js
+++ controllers/series.js
@@ -11,25 +11,41 @@ const index = async ({ Serie }, req, res) => {
 
 const novaProcess = async ({ Serie }, req, res) => {
   const serie = new Serie(req.body)
-  await serie.save()
-  res.redirect('/series')
+  try {
+    await serie.save()
+    res.redirect('/series')
+  } catch (e) {
+    res.render('series/nova', {
+      errors: Object.keys(e.errors)
+    })
+  }
 }
 
 const novaForm = (req, res) => {
-  res.render('series/nova')
+  const errors = []
+  res.render('series/nova', { errors })
 }
 
 const editarProcess = async ({ Serie }, req, res) => {
   const serie = await Serie.findOne({ _id: req.params.id })
   serie.name = req.body.name
   serie.status = req.body.status
-  await serie.save()
-  res.redirect('/series')
+  try {
+    await serie.save()
+    res.redirect('/series')
+  } catch (e) {
+    res.render('series/editar', {
+      serie,
+      labels,
+      errors: Object.keys(e.errors)
+    })
+  }
 }
 
 const editarForm = async ({ Serie }, req, res) => {
+  const errors = []
   const serie = await Serie.findOne({ _id: req.params.id })
-  res.render('series/editar', { serie, labels })
+  res.render('series/editar', { serie, labels, errors })
 }
 
 const excluir = async ({ Serie }, req, res) => {
--- models/serie.js
+++ models/serie.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const SerieSchema = mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true
+  },
   status: {
     type: String,
     enumValues: [ 'to-watch', 'watching', 'watched' ]
